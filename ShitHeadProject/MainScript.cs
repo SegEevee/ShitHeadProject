3ba34db26a309e564a2db1352bd437b180a6558e
SegEevee <segev.jacobi1@gmail.com>
2025-05-23 19:49:57 +0300

    created the sln

---
+++ ShitHeadProject/MainScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShitHead
+{
+    internal class MainScript
+    {
+        static void Main(string[] args) {
+        }
+    }
+}
